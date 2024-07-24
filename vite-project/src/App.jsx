4f749d69c1e0757c7a11f3e8a3b2b1fcce76b845
DevRamona <r.ingabire@alustudent.com>
2024-07-24 12:29:04 +0200

    new changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,12 +5,20 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const Add = () => {
+  const [notification, setNotification] = useState([])
+
+  // function updateNotifications () {
+  //   setNotification(prevNotification => !prevNotification)
+  // } 
+  const add = () => {
     setCount(prevCount => prevCount + 1) 
   } 
-  const Subtract = () => {
-    setCount(prevCount => prevCount - 1 )
+  const subtract = () => {
+    count >= 1 ? setCount(count-1) :count
     
+  }
+  const reset = () => {
+    setCount(0)
   }
 
   return (
@@ -18,10 +26,20 @@ function App() {
     <div className='w-3/5 relative '>
     <h1 className='border bg-gray-300 rounded-lg p-8 mx-auto w-36'>{count}</h1>
     <div className='flex space-x-5 justify-center absolute left-0 right-0 -bottom-14 text-3xl '>
-    <button className='p-5 bg-lime-50  rounded-2xl ' onClick={Subtract}> Decrease</button>
-    <button className='p-4 bg-indigo-100 rounded-2xl ' onClick={Add}> Increase</button>
+    <button className='p-5 bg-lime-50  rounded-2xl ' onClick={subtract}> Decrease</button>
+    <button className='p-4 bg-indigo-100 rounded-2xl ' onClick={add}> Increase</button>
+    <button className='p-4 bg-indigo-100 rounded-2xl ' onClick={reset}> reset</button>
     </div>
-      
+
+   
+    </div>
+    <div>
+       {
+      notification.length === 0 ?
+      <h6>You're all caught up!</h6> :
+      <h6>You have {notification.length} unread {notification.length > 1 ? "notifications" : "notification"}</h6>
+        }
+    
     </div>
    
     
